18b401345f4d9a49a7fa16c0b160d5c3054f7395
Rob Zwissler <rob@zwissler.org>
2024-04-09 09:49:10 -0700

    switched shfmt to -i2

--- scripts/lint.bash
+++ scripts/lint.bash
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 shellcheck --shell=bash --external-sources \
-	bin/* --source-path=template/lib/ \
-	lib/* \
-	scripts/*
+  bin/* --source-path=template/lib/ \
+  lib/*.bash \
+  lib/commands/*.bash \
+  scripts/* \
+  asdf-sync
 
-shfmt --language-dialect bash --diff \
-	./**/*
+shfmt --language-dialect bash -i 2 --diff \
+  asdf-sync ./**/*
